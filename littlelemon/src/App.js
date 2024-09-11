86987528ea366b81a9215cdc669f70c5a5d8d647
Arvind Manivannan <arvindmanivannan993@gmail.com>
2024-09-11 07:23:04 +0530

    First Commit

---
+++ littlelemon/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <p>This is a simple React app with a custom webpack configuration.</p>
+    </div>
+  );
+}
+
+export default App;
